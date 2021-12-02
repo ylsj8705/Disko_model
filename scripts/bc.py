55922fe84f9f3757c8b530e839bed2cd1845111f
Ylva Sjöberg <ys@ign.ku.dk>
2021-12-02 19:49:37 +0100

    more snow and inflow input data

--- scripts/bc.py
+++ scripts/bc.py
@@ -8,25 +8,51 @@ import matplotlib.pyplot as plt
 timstep = 86400 # Timestep for output in seconds
 
 data = np.genfromtxt('../data_raw/spinup_data.txt', dtype=float, usecols=(2,3,4,5,7,8), delimiter=" ", skip_header=1)
-sw = list(map(float, data[:,3]))
-RH = list(map(float, data[:,1]))
-wind = list(map(float, data[:,2]))
+sw = data[:,3]
+RH = data[:,1]
+wind = data[:,2]
 Ps = data[:,5]
-Pr = list(map(float, data[:,4]))
-AT = list(map(float, data[:,0]))
+Pr = data[:,4]
+AT = data[:,0]
 
-Ps = Ps*3
+
+# Run data
+data = np.genfromtxt('../data_raw/DSK_WeatherDriver_21-11-17_1728_COMBI.txt', dtype=float, usecols=(1,2,3,4,6,7), delimiter=" ", skip_header=1)
+#sw_r = data[:,3]
+#RH_r = data[:,1]
+#wind_r = data[:,2]
+Ps_r = data[:,5]
+Pr_r = data[:,4]
+AT_r = data[:,0]
+
+AT_2016 = AT_r[5844:6210]
+Ps_2016 = Ps_r[5844:6210]
+Pr_2016 = Pr_r[5844:6210]
+
+# make series combing 10 yrs spinup and then run data
+AT = np.concatenate((AT[0:3650], AT_2016))
+Ps = np.concatenate((Ps[0:3650], Ps_2016))
+Pr = np.concatenate((Pr[0:3650], Pr_2016))
+
+# Vary precipitation
+Ps_3, Ps_4, Ps_5, Ps_6 = Ps*3, Ps*4, Ps*5, Ps*6
+Pr = Pr*1.2
+
 ### Time stamp
-dys = len(sw)
+dys = len(AT)
 lastday = (timstep*dys)-timstep
 Time = np.linspace(0,lastday,dys)  #  in seconds, daily time step
 
 hf = h5py.File('../data_processed/spinup_disko.h5', 'w')
 hf.create_dataset('Ta', data=AT)
-hf.create_dataset('Us', data=wind)
+hf.create_dataset('Us', data=wind[0:4016])
 hf.create_dataset('Ps', data=Ps)
+hf.create_dataset('Ps_3', data=Ps_3)
+hf.create_dataset('Ps_4', data=Ps_4)
+hf.create_dataset('Ps_5', data=Ps_5)
+hf.create_dataset('Ps_6', data=Ps_6)
 hf.create_dataset('Pr', data=Pr)
-hf.create_dataset('RH', data=RH)
-hf.create_dataset('Qswin', data=sw)
+hf.create_dataset('RH', data=RH[0:4016])
+hf.create_dataset('Qswin', data=sw[0:4016])
 hf.create_dataset('time', data=Time)
 hf.close()
